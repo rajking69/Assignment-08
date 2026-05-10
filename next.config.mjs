fee33e6f9cfebab3c2b353bb0e3b5516a65294ef
rajking_69 <smrajking4457@gmail.com>
2026-05-11 00:58:20 +0600

    initialize project config and metadata

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.postimg.cc",
+        pathname: "/**",
+      },
+    ],
+  },
+  turbopack: {
+    root: __dirname,
+  },
+  serverExternalPackages: ["mongodb"],
+};
+
+export default nextConfig;
